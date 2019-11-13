ee17c631acca1d4fff5e43b05d640a209e0424ba
Svilen Velikov <svilen.velikov@sirma.bg>
2019-11-13 18:04:12 +0200

    add mode, dao, services, controllers

---
+++ src/main/java/com/springsec/springboottest/api/PersonController.java
@@ -0,0 +1,55 @@
+package com.springsec.springboottest.api;
+
+import com.springsec.springboottest.model.Person;
+import com.springsec.springboottest.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
+@RestController
+public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(@Valid @NonNull @RequestBody Person person) {
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id) {
+        return personService.getPersonById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletePersonById(@PathVariable("id") UUID id) {
+        personService.deletePerson(id);
+    }
+
+    @PutMapping("{id}")
+    public void updatePersonById(@PathVariable("id") UUID id, @Valid @NonNull @RequestBody Person person) {
+        personService.updatePerson(id, person);
+    }
+}
